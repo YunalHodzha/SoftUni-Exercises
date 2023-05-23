fafcbe2219e2c342b28e6199340224ae4b531bc6
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-05-23 18:34:36 +0300

    update

--- Advanced/04.DOM Introduction/exe/07. Hell's Kitchen/app.js
+++ Advanced/04.DOM Introduction/exe/07. Hell's Kitchen/app.js
@@ -2,7 +2,16 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      //   TODO:
+      const obj = {};
+
+      const input = document.querySelector("#inputs textarea");
+      let inputList = input.value.replaceAll("\n", " ");
+
+      for (let element of inputList) {
+         console.log(element)
+      }
+
+      console.log({inputList})
       
    }
 }
