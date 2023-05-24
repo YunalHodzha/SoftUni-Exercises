0dcb72b1ce0bb594f48be84c896c7253d8e3a11e
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-05-24 18:04:39 +0300

    update

--- Advanced/05. JS-Advanced-DOM-Events-Lab-Resources/05. Highlight-Active/app.js
+++ Advanced/05. JS-Advanced-DOM-Events-Lab-Resources/05. Highlight-Active/app.js
@@ -1,3 +1,19 @@
 function focused() {
-    console.log('TODO:...');
+    const sections = document.querySelectorAll("body div div");
+
+    for(let section of sections) {
+        let textInput = section.getElementsByTagName("input")[0];
+        textInput.addEventListener("focus", focus);
+        textInput.addEventListener("blur", focusLost);
+    }
+
+    function focus(e) {
+        let parent = e.target.parentNode;
+        parent.classList.add("focused");
+    }
+
+    function focusLost(e) {
+        let parent = e.target.parentNode;
+        parent.classList.remove("focused");
+    }
 }
--- Advanced/05. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
+++ Advanced/05. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
@@ -1,3 +1,14 @@
 function solve() {
-   //TODO...
+   const products = document.getElementsByClassName("product");
+   
+   
+   for(let i = 0; i <products.length; i++) {
+      let product_add = products[i].children[2];
+      let addBtn = product_add.getElementsByTagName("button")[0];
+
+      console.log(addBtn)
+   }
+
+
+   console.log(products);
 }
