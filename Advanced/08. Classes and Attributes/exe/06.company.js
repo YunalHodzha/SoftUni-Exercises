464dd5b8818ed79eee609fb2791469eadda7611b
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-06-04 23:54:37 +0300

    update 06.company.js

--- Advanced/08. Classes and Attributes/exe/06.company.js
+++ Advanced/08. Classes and Attributes/exe/06.company.js
@@ -12,20 +12,55 @@ class Company {
             throw new Error("Invalid input!");
         }
 
-        this.departments[name] = { salary: salary, position: position, department: department };
+        if (!this.departments[department]) {
+            this.departments[department] = [];
+        }
+
+        this.departments[department].push({name: name, salary: salary, position: position, department: department });
         return `New employee is hired. Name: ${name}. Position: ${position}`;
     }
 
     bestDepartment() {
-        let array = Array.from(this.departments)
-        console.table(array)
+        let bestDepartment;
+        let bestAvgSalary = 0;
 
+        for (let department in this.departments) {
+            const employees = this.departments[department];
+            let totalSalarySum = 0;
+            let counter = 0;
+
+            for(let employee of employees) {
+                totalSalarySum += employee.salary;
+                counter++;
+            }
+
+            let currentAvg = (totalSalarySum / counter).toFixed(2);
+            if(currentAvg > bestAvgSalary) {
+                bestAvgSalary = currentAvg;
+                bestDepartment = department;
+            }
+        }
+
+        let sortedEmployees = this.departments[bestDepartment];
+        sortedEmployees.sort((a,b) => {
+            if(a.salary === b.salary) {
+                return a.name.localeCompare(b.name);
+            }
+           return b.salary - a.salary
+        })
+
+        let result = [`Best Department is: ${bestDepartment}`, `Average salary: ${bestAvgSalary}`];
+        sortedEmployees.forEach(element => {
+            result.push(`${element.name} ${element.salary} ${element.position}`);
+        });
+
+        return result.join("\n");
     }
 
 }
 
 let c = new Company();
-console.log(c.addEmployee("Stanimir", 2000, "engineer", "Construction"))
+c.addEmployee("Stanimir", 2000, "engineer", "Construction")
 c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
 c.addEmployee("Slavi", 500, "dyer", "Construction");
 c.addEmployee("Stan", 2000, "architect", "Construction");
