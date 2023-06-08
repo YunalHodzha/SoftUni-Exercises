f88ed075edc9abe10c50dcd3e2abaf74fbe6f9aa
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-06-08 17:13:40 +0300

    update

--- Advanced/09. Prototypes and Inheritance/lab/05.ClassHierarchy.js
+++ Advanced/09. Prototypes and Inheritance/lab/05.ClassHierarchy.js
@@ -88,7 +88,7 @@ function Hierarchy() {
     }
     let c = new Circle(5);
     console.log(c.area); // 78.53981633974483
-    console.log(c.toString()); // Figures units: cm Area: 78.53981633974483 - radius: 5
+    xconsole.log(c.toString()); // Figures units: cm Area: 78.53981633974483 - radius: 5
     let r = new Rectangle(3, 4, 'mm');
     console.log(r.area); // 1200
     console.log(r.toString()); //Figures units: mm Area: 1200 - width: 30, height: 40
