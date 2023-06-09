b2ed27e6ad8f5d491d58a04350bd1de44e49cf97
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-06-09 20:56:22 +0300

    update

--- Advanced/09. Prototypes and Inheritance/exe/06.posts.js
+++ Advanced/09. Prototypes and Inheritance/exe/06.posts.js
@@ -6,14 +6,59 @@ function solution() {
         }
 
         toString() {
-            return myPost = {
-                
+            return `Post: ${this.title}\nContent: ${this.content}`;
+        }
+    }
+
+    class SocialMediaPost extends Post {
+        constructor(title, content, likes, dislikes) {
+            super(title, content);
+            this.likes = likes;
+            this.dislikes = dislikes;
+            this.comments = [];
+        }
+
+        addComment(comment) {
+            this.comments.push(comment);
+        }
+
+        toString() {
+            if (this.comments.length > 0) {
+                let myComments = "";
+                for (let comment of this.comments) {
+                    myComments += ` * ${comment}\n`;
+                }
+                return `${super.toString()}\nRating: ${this.likes - this.dislikes}\nComments:\n${myComments}`.trim()
             }
+
+            return `${super.toString()}\nRating: ${this.likes - this.dislikes}`;
+        }
+    }
+
+    class BlogPost extends Post {
+        constructor(title, content, views) {
+            super(title, content);
+            this._views = Number(views);
+        }
+
+        get views() {
+            return this._views;
+        }
+
+        view() {
+            this._views++;
+            return this;
+        }
+
+        toString() {
+            return `${super.toString()}\nViews: ${this._views}`;
         }
     }
 
     return {
-        Post
+        Post,
+        SocialMediaPost,
+        BlogPost
     }
 }
 
@@ -27,10 +72,6 @@ scm.addComment("Good post");
 scm.addComment("Very good post");
 scm.addComment("Wow!");
 console.log(scm.toString());
-// Post: TestTitle
-// Content: TestContent
-// Rating: -5
-// Comments:
-// * Good post
-// * Very good post
-// * Wow!
+
+let vv = new classes.BlogPost("TestTestov", "Contententov", 888);
+console.log(vv.toString())
