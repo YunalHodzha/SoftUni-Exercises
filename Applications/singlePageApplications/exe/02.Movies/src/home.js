a77ba360b1a0c7eb98ed637c4d9b3e2436c5afde
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-07-08 10:09:05 +0300

    Update home.js

--- Applications/singlePageApplications/exe/02.Movies/src/home.js
+++ Applications/singlePageApplications/exe/02.Movies/src/home.js
@@ -1,5 +1,15 @@
 import { showView } from "./utils.js";
 const section = document.getElementById('home-page');
+const catalog = document.querySelector('#movie .card-deck.d-flex.justify-content-center');
+
+catalog.addEventListener('click', (event) => {
+    if(event.target.tagName === 'BUTTON') {
+        event.preventDefault();
+
+        console.log('Event')
+    }
+});
+
 
 export function homePage() {
     console.log('...homePage...');
@@ -9,4 +19,38 @@ export function homePage() {
     //- display only movies section (request + ...)
 
     showView(section);
+
+    displayMovies()
+}
+
+async function displayMovies() {
+
+    const movies = await getMovies();
+
+    catalog.replaceChildren(...movies.map(createMoviePreview));
+
+}
+
+function createMoviePreview(movie) {
+    const liElem = document.createElement('li');
+    liElem.innerHTML = `
+    <img class="card-img-top" src="${movie.img}" alt="Card image cap" width="400">
+    <div class="card-body">
+        <h4 class = "card-title">${movie.title}</h4>
+        <a href="/details/${movie._id}">
+            <button data-id="${movie._id}" type="button" class="btn btn-info">Details</button>
+        </a>
+    </div>
+    <div class="card-footer">
+    </div>    
+    `;
+
+    return liElem;
+}
+
+async function getMovies() {
+    const res = await fetch('http://localhost:3030/data/movies');
+    const movies = await res.json();
+
+    return movies;
 }
