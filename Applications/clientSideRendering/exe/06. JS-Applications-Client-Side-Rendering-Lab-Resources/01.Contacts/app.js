2ebe77f5e5ba18ec2d3a887466fe3e3f851586a7
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-07-11 18:31:15 +0300

    update

--- Applications/clientSideRendering/exe/06. JS-Applications-Client-Side-Rendering-Lab-Resources/01.Contacts/app.js
+++ Applications/clientSideRendering/exe/06. JS-Applications-Client-Side-Rendering-Lab-Resources/01.Contacts/app.js
@@ -1,22 +1,29 @@
+import { html, render } from "./node_modules/lit-html/lit-html.js";
 import { contacts } from "./contacts.js";
-let regex = /%%([a-zA-Z]*)%%/gi;
+
 
 let main = document.getElementById('contacts');
 
-let response = await fetch('./contactTemplate.html');
-let contactTemplate = await response.text();
 
-for (const contact of contacts) {
-    let populatedTemplate = populateTemplate(contactTemplate, contact)
-    main.innerHTML += populatedTemplate;
-}
+let contactTemplate = (contact) => html`
+<div class="contact card">
+    <div>
+        <i class="far fa-user-circle gravatar"></i>
+    </div>
+    <div class="info">
+        <h2>Name: ${contact.name}</h2>
+        <button class="detailsBtn">Details</button>
+        <div class="details" id="${contact.id}">
+            <p>Phone number: ${contact.phoneNumber}</p>
+            <p>Email: ${contact.email}</p>
+        </div>
+    </div>
+</div>`;
 
+let contactsTemplate = (contacts) => html`
+<main>
+    ${contacts.map(x => contactTemplate(x))}
+</main>
+    `
 
-
-function populateTemplate(template, data) {
-    template = template.replace(regex, (match, parameterName) => {
-        return data[parameterName];
-    });
-    return template;
-
-}
+render(contactsTemplate(contacts), main);
