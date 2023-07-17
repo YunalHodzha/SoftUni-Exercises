6f9e01386b106ee520f095f5d7e31d10ede51d76
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-07-17 19:00:50 +0300

    update

--- Applications/examPreps/Clear/src/views/home.js
+++ Applications/examPreps/Clear/src/views/home.js
@@ -1,8 +1,15 @@
 import { html } from "../../node_modules/lit-html/lit-html.js";
 
 const homeTemplate = () => html`
-<h1>Home Page</h1>
-<p>Welcome to our site</p>`;
+<!-- Home page -->
+<section id="home">
+          <img
+            src="./images/pngkey.com-hunting-png-6697165-removebg-preview.png"
+            alt="home"
+          />
+          <h2>Searching for a job?</h2>
+          <h3>The right place for a new career start!</h3>
+        </section>`;
 
 export function homePage(ctx) {
     ctx.render(homeTemplate());
