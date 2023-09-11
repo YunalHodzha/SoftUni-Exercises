949afb45c90bb3bb03ef9461271be24ad5c74cb1
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-09-11 18:24:49 +0300

    update

--- JS-Web/01.introToNodejs/Cat Shelter/handlers/home.js
+++ JS-Web/01.introToNodejs/Cat Shelter/handlers/home.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const cats = require('../data/cats');
 
 const homePath = (path.join(__dirname, '../views/home/index.html'));
 const homeHtmlContent = fs.readFileSync(homePath, 'utf-8');
